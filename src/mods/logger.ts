4eb49de25e8ede7a96cd69200bd449d8bb2851e9
tabliang <tabliang@tencent.com>
2020-08-31 17:06:18 +0800

    feat: supply base constructor

--- src/mods/logger.ts
+++ src/mods/logger.ts
@@ -1,8 +1,71 @@
 export default class Logger {
+  private config: Object;
+
+  private levels: {
+    debug: 0;
+    log: 1;
+    info: 2;
+    warn: 3;
+    error: 4;
+  };
+
+  private colors: {
+    log: '';
+    info: 'blue';
+    debug: 'gray';
+    error: 'red';
+    warn: 'yellow';
+    success: 'green';
+    fail: 'red';
+    tip: 'cyan';
+    stress: 'magenta';
+  };
+
+  private icons: {
+    log: {
+      icon: '.';
+      color: '';
+    };
+    info: {
+      icon: '*';
+      color: 'blue';
+    };
+    debug: {
+      icon: '#';
+      color: 'gray';
+    };
+    warn: {
+      icon: '!';
+      color: 'yellow';
+    };
+    error: {
+      icon: 'x';
+      color: 'red';
+    };
+    success: {
+      icon: '✓';
+      color: 'green';
+    };
+    fail: {
+      icon: '☢';
+      color: 'red';
+    };
+    tip: {
+      icon: '✱';
+      color: 'cyan';
+    };
+    stress: {
+      icon: '⚑';
+      color: 'magenta';
+    };
+  };
+
   private constructor(options) {
-    console.log('init');
-    this.config(options);
+    this.set(options);
+    this.init();
   }
+
+  public output(options: any, para: Array<any>) {}
 
   // clone logger
   public fork(options) {
@@ -11,9 +74,41 @@ export default class Logger {
     return clone;
   }
 
-  // set colors
-  public theme(spec) {}
+  // set theme
+  public theme(spec: any) {
+    if (spec) {
+      ['colors', 'icons', 'levels'].forEach((prop) => {
+        if (typeof spec[prop] === 'object') {
+          Object.assign(this[prop], spec[prop]);
+        }
+      });
+    }
+  }
 
-  // set meta message
-  public config(options) {}
+  // set config
+  public set(options) {
+    this.config = {
+      ...options,
+    };
+  }
+
+  // add custom method
+  public method(name: string, options) {
+    this[name] = (...para) => {
+      this.output(options, para);
+    };
+  }
+
+  private init() {
+    Object.keys(this.levels).forEach((level) => {
+      this.method(level, {
+        level,
+      });
+    });
+    ['success', 'fail', 'tip', 'stress'].forEach((prop) => {
+      this.method(prop, {
+        level: 'log',
+      });
+    });
+  }
 }
