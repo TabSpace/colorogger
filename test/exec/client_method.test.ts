e8305dde6c1b0be09b66725c8eb1e6fdcff2e8d4
tabliang <tabliang@tencent.com>
2020-09-03 17:20:10 +0800

    test: update client tests

---
+++ test/exec/client_method.test.ts
@@ -0,0 +1,64 @@
+import $assert from 'power-assert';
+import $logger from '../../src/client';
+
+interface CustomLogger extends $logger {
+  temp?: Function;
+  custom?: Function;
+}
+
+let msg = null;
+
+const logger: CustomLogger = new $logger({
+  transport: (message) => {
+    msg = message;
+  },
+});
+
+logger.method('temp');
+logger.method('custom', {
+  flag: 'custom',
+});
+
+logger.theme({
+  colors: {
+    custom: '#ff9501',
+  },
+  icons: {
+    custom: {
+      icon: 'c',
+      color: '#ff9501',
+    },
+  },
+});
+
+describe('temp method', () => {
+  beforeAll(() => {
+    logger.temp('t1');
+  });
+
+  test('temp msg.content', () => {
+    $assert.equal(msg.content[0], 't1');
+  });
+  test('temp icon', () => {
+    $assert.equal(msg.__content[2], 'color: ;');
+  });
+  test('temp color', () => {
+    $assert.equal(msg.__content[3], 'color: ;');
+  });
+});
+
+describe('custom method', () => {
+  beforeAll(() => {
+    logger.custom('c1');
+  });
+
+  test('custom msg.content', () => {
+    $assert.equal(msg.content[0], 'c1');
+  });
+  test('custom icon', () => {
+    $assert.equal(msg.__content[2], 'color: #ff9501;');
+  });
+  test('custom color', () => {
+    $assert.equal(msg.__content[3], 'color: #ff9501;');
+  });
+});
---
+++ test/exec/client_theme.test.ts
@@ -0,0 +1,62 @@
+import $assert from 'power-assert';
+import $logger from '../../src/client';
+
+let msg = null;
+
+const logger = new $logger({
+  transport: (message) => {
+    msg = message;
+    console.log(msg);
+  },
+});
+
+logger.theme({
+  colors: {
+    info: 'yellow',
+    debug: '#ff9501',
+  },
+  icons: {
+    info: {
+      icon: 'i',
+      color: '',
+    },
+    debug: {
+      icon: 'd',
+      color: '#ff9501',
+    },
+  },
+});
+
+describe('info theme', () => {
+  beforeAll(() => {
+    logger.info('info');
+  });
+
+  test('info msg.content', () => {
+    $assert.equal(msg.content[0], 'info');
+  });
+  test('info icon', () => {
+    $assert(msg.__content[0].indexOf('%c[i]') > 0);
+    $assert.equal(msg.__content[2], 'color: ;');
+  });
+  test('info color', () => {
+    $assert.equal(msg.__content[3], 'color: yellow;');
+  });
+});
+
+describe('debug theme', () => {
+  beforeAll(() => {
+    logger.debug('debug');
+  });
+
+  test('debug msg.content', () => {
+    $assert.equal(msg.content[0], 'debug');
+  });
+  test('debug icon', () => {
+    $assert(msg.__content[0].indexOf('%c[d]') > 0);
+    $assert.equal(msg.__content[2], 'color: #ff9501;');
+  });
+  test('debug color', () => {
+    $assert.equal(msg.__content[3], 'color: #ff9501;');
+  });
+});
