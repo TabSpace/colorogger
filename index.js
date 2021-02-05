224d1c04f440231c28bb548babc8665055d11007
tabliang <tabliang@tencent.com>
2021-02-05 11:29:13 +0800

    style: improve index file style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable */
 
 const $logger = require('./lib/server');
+
 const logger = $logger.default;
 logger.default = $logger.default;
 module.exports = logger;
